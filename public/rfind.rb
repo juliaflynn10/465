ca257506a23bb8a5c50ed5db153f9f79ec42b825
thecodelifechoseme <jflynn2@mail.csuchico.edu>
2014-09-09 00:45:38 -0700

    fixed formatting, added grep

--- public/rfind.rb
+++ public/rfind.rb
@@ -5,19 +5,20 @@
 
 #!/usr/bin/env ruby
 
-:stars = 50 * "*"
-:dashes = 50 * "-"
-:tabs  = 2 * " "
+stars = 50 * "*"
+dashes = 50 * "-"
+tabs  = 2 * " "
 
 
-key = gets
-all_files = (`find .`).split
-wanted_types = all_files.select {|file| file =~ /.rb|.erb|.js|.css|.html|.yml|.txt/}
+key = gets #store searchable word
+all_files = (`find .`).split #find all files (with their paths) store as strings rather than array
 
-matched_titles = wanted_types.select { |exp| exp =~ /\S*#{key}\S*/}
+puts "Files with names that matches <" + key + ">"
+wanted_types = all_files.select {|file| file =~ /.rb|.erb|.js|.css|.html|.yml|.txt/} #filter only files that we want to search, generate an array
+matched_titles = wanted_types.select { |exp| exp =~ /\S*#{key}\S*/} #look in those files' titles for the key word, generate an array
+matched_titles.each {|i| p tabs + i} #print results with 2 spaces
+puts stars #print 50 stars
 
 
-
-puts "Files with content that matches <" + key + ">"
-matched_titles.each {|i| p i}
-puts :stars
+puts "Files with content that matches <" + key + ">" #print this
+grepped = wanted_types.each {|path| `grep -i -n #{key} #{path}`}.split("\n")
