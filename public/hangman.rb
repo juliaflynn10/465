7501199308df5200a6b1f3d806e2f6e7f152aa49
thecodelifechoseme <jflynn2@mail.csuchico.edu>
2014-09-18 08:29:49 -0700

    updated find words of length of x, user input & output

--- public/hangman.rb
+++ public/hangman.rb
@@ -2,15 +2,21 @@
 #CINS 465
 #Project 2
 
-
 #!/usr/bin/env ruby
 
-full_dic = Array.new
+TOTAL_GUESSES = 10
+failed = Array.new
+passed = Array.new
+dic = Array.new
+
+#push each line of a file into an array, deleting the \n chr
 File.open('words', 'r') do |file|
-    file.each_line{|line| full_dic.push(line.chomp)}
+    file.each_line{|line| dic.push(line.chomp)}
 end
 
-put "Choose a number between 5 and 20"
+#prompt user
+puts "Choose a number between 5 and 20"
+#get input, convert to int
 word_length = gets.chomp.to_i
 
 if word_length < 5
@@ -19,9 +25,43 @@ elsif word_length > 20
     word_length = 20
 end
 
-full_dic.map!{|x| x if x.size == word_length}.compact
+#delete words that aren't of the specified length from the dictionary
+dic.map!{|x| x if x.size == word_length.compact}
+
+#while the user hasn't run out of guesses and the word hasn't been completed, allow guesses
+while failed.length < TOTAL_GUESSES && passed.length < word_length
+    puts "Choose a letter. You have " + (TOTAL_GUESSES - failed.length) + " guess(es) remaining."
+    letter = gets.chr
+    #while a letter has been guessed
+    while failed.include?(letter) || passed.include?(letter)
+        puts "You already guessed that, try again. Choose a letter."
+    end
+    #if you can't remove all words that contain the guessed character without the dictionary being empty
+    if dic.map{|word| word if !word.include?(letter)}.compact.empty?
+        passed.insert(dic[0].index(letter), letter)
+    else
+        failed.push(letter)
+    end
+
+    failed_guesses = failed.each{|x| print x, " "}
+    passed_guesses = passed.each_chr do |x| 
+        if x 
+            print x 
+        else 
+            print '_'
+        print " "
+        end
+    end
+    puts "INCORRECT GUESSES: " + failed_guesses
+    puts "MYSTERY WORD: " + passed_guesses
+end
+
+if passed_guesses.length == word_length
+    puts "You Won!"
+else
+    puts "You Lost"
+end
 
 
-    
 
 
