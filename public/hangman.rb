e77725d7131153b64c48de81364a7f6ed294a57f
thecodelifechoseme <jflynn2@mail.csuchico.edu>
2014-09-18 11:21:07 -0700

    working on passed's output

--- public/hangman.rb
+++ public/hangman.rb
@@ -5,8 +5,6 @@
 #!/usr/bin/env ruby
 
 TOTAL_GUESSES = 10
-failed = Array.new
-passed = Array.new
 dic = Array.new
 
 #push each line of a file into an array, deleting the \n chr
@@ -27,9 +25,10 @@ end
 
 #delete words that aren't of the specified length from the dictionary
 dic.map!{|x| x if x.size == word_length}.compact!
-
+failed = Array.new
+passed = Array.new(word_length)
 #while the user hasn't run out of guesses and the word hasn't been completed, allow guesses
-while failed.length < TOTAL_GUESSES && passed.length < word_length
+while failed.length < TOTAL_GUESSES && passed.compact.length < word_length
     puts "Choose a letter. You have " + (TOTAL_GUESSES - failed.length).to_s + " guess(es) remaining."
     letter = gets.chr
     #while a letter has been guessed
@@ -40,7 +39,8 @@ while failed.length < TOTAL_GUESSES && passed.length < word_length
     #if you can't remove all words that contain the guessed character without the dictionary being empty
     if dic.map{|word| word if !word.include?(letter)}.compact.empty?
         offset = 0
-        while word_length > offset
+        while word_length > offset && offset != nil
+            passed.delete_at(dic[0].index(letter, offset))
             passed.insert(dic[0].index(letter, offset), letter)
             offset = dic[0].index(letter, offset + 1)
         end
@@ -59,10 +59,11 @@ while failed.length < TOTAL_GUESSES && passed.length < word_length
         print " "
     end
     puts "INCORRECT GUESSES: " + failed_guesses.to_s
+    puts '\n'
     puts "MYSTERY WORD: " + passed_guesses.to_s
 end
 
-if passed_guesses.length == word_length
+if passed_guesses.compact.length == word_length
     puts "You Won!"
 else
     puts "You Lost"
