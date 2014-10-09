ae14ed9b4a15ad433d4d56623580f0ec28fb8e26
Julia Flynn <thecodelifechoseme@gmail.com>
2014-10-09 18:37:02 +0000

    show view displays guesses

--- p3/wladder/app/controllers/home_controller.rb
+++ p3/wladder/app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
  	 end
 
   	def show
-		@guesses_array = params[:guesses]
+		@guesses_array = params.permit(:step0, :step1, :step2, :step3, :step4, :step5, :step6).map{|guess| guess[1]}
 	end
 end
 
