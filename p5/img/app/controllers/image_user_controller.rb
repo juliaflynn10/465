fe2fabbd1777ae827df71321b8189e14005124c3
Julia Flynn <thecodelifechoseme@gmail.com>
2014-11-19 06:05:54 +0000

    finished controllers

--- p5/img/app/controllers/image_user_controller.rb
+++ p5/img/app/controllers/image_user_controller.rb
@@ -2,19 +2,19 @@
 # since ratings are shown by professor, it does not make sense to
 # show a single rating
 
-class ImageObjectsController < ApplicationController
-  before_action :set_image_object, only: [:edit, :update, :destroy]
+class ImageUsersController < ApplicationController
+  before_action :set_image_user, only: [:edit, :update, :destroy]
 
   # GET /professors/:professor_id/ratings
   def index
     # index is now all the ratings for a single professor (not all the ratings)
     # the URL will contain the id of the current professor (:professor_id)
     # use the :professor_id parameter to find all the ratings
-    @user = User.find params[:user_id]
+    @image_object = ImageObject.find params[:image_object_id]
     # the has_many function called in app/models/professor.rb created the 
     # function "ratings" which will return an array of all the ratings 
     # with the professor foreign key matching @professor
-    @image_objects = @user.image_objects
+    @image_users = @image_object.image_users
   end
 
   # GET professors/:professor_id/ratings/new
@@ -24,11 +24,10 @@ class ImageObjectsController < ApplicationController
   def new
     # since our the rating new path contains the professor's id
     # we can use params[:professor_id] to get that id
-    @user = User.find params[:user_id]
-
+    @image_object = ImageObject.find params[:image_object_id]
     # This is similar to Rating.new, BUT it creates the new rating
     # in the context of a Professor object and sets the foreign key
-    @image_object = @user.image_objects.new
+    @image_user = @image_object.image_user.new
   end
 
   # GET /ratings/1/edit
@@ -40,33 +39,23 @@ class ImageObjectsController < ApplicationController
   # we need the professor's key in the URL to make sure that someone 
   # isn't trying to hack the id of the new rating's professor
   # rails ensures that the URL has not be tampered with
-
-
-
-  # POST /images
   def create
-    @image_object = ImageObject.new(image_object_params)
-    @image_object.generate_filename
-    @image.user = current_user
+    @image_object = ImageObject.find params[:image_object_id]
+    @image_user = @ImageObject.image_users.new(image_user_params)
 
-    @uploaded_io = params[:image][:uploaded_file]
-
-    File.open(Rails.root.join('public', 'images', @image.filename), 'wb') do |file|
-        file.write(@uploaded_io.read)
-    end
-
-    if @image.save
-      redirect_to @image, notice: 'Image was successfully created.'
+    if @image_user.save
+      redirect_to image_object_image_users_url(@image_object) , notice: 'ImageUser was successfully created.'
     else
       render :new
     end
   end
+
   # PATCH/PUT /ratings/1
   # updates don't have to be nested because the professor foreign key is already set
   # and cannot be changed by edit (note that professor_id is not permitted in rating_params())
   def update
-    if @image_object.update(image_object_params)
-      redirect_to user_image_objects_url(@image_object.user), notice: 'ImageObject was successfully updated.'
+    if @image_user.update(image_user_params)
+      redirect_to image_object_image_users_url(@image_user.image_object), notice: 'ImageUser was successfully updated.'
     else
       render :edit
     end
@@ -74,18 +63,18 @@ class ImageObjectsController < ApplicationController
 
   # DELETE /ratings/1
   def destroy
-    @image_object.destroy
-    redirect_to user_image_objects_url(@image_object.user , notice: 'ImageObject was successfully destroyed.'
+    @image_user.destroy
+    redirect_to image_object_image_users_url(@image_user.image_object) , notice: 'ImageUser was successfully destroyed.'
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_image_object
-      @image_object = ImageObject.find(params[:id])
+    def set_image_user
+      @image_user = ImageUser.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def image_object_params
-      params.require(:image_object).permit(:filename, :private, :user_id)
+    def image_user_params
+      params.require(:image_user).permit(:image_object_id, :user_id)
     end
 end
