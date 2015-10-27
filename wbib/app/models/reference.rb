5d01cd8312d1d6399d00ed1fff05aa2baf26e84d
Julia Flynn <jflynn2@mail.csuchico.edu>
2015-10-26 21:58:35 -0700

    added wbib

--- wbib/app/models/reference.rb
+++ wbib/app/models/reference.rb
@@ -1,6 +1,17 @@
 class Reference < ActiveRecord::Base
   validates :URL, presence: true
   validates :date, presence: true
+  before_save :validate_url
+ 
+  validates_format_of :URL, :with => /((?:https?\:\/\/|www\.)(?:[-a-z0-9]+\.)*[-a-z0-9]+.*)/i
 
   belongs_to :topic
+
+  def validate_url
+	if self.URL.to_s =~ /\A(https?\:\/\/.*)\z/
+		self.URL = self.URL
+	else
+		self.URL = "http://" + self.URL
+	end
+  end
 end
